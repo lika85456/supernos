2b54b0b47db5e42c13718fa10901840ed94a2ee4
lika85456 <lika85456@post.cz>
2017-04-11 13:03:10 +0200

    Update BattlePacketHandler.java

--- src/nostale/handler/BattlePacketHandler.java
+++ src/nostale/handler/BattlePacketHandler.java
@@ -9,11 +9,11 @@ public class BattlePacketHandler extends Handler{
         this.n = n;
     }
     
-    public String UseSkill(Skill s,MapMobInstance m)
+    public void UseSkill(Skill s,MapMobInstance m)
     {
-    //{CastId} {UserType} {MapMonsterId} {MapX} {MapY}
-    this.lastSkillRequest = s;
-    n.send("u_s "+s.CastId+" 3 "+m.id+" "+n.GameData.Character.Pos.x+" "+n.GameData.Character.Pos.y);
+        //{CastId} {UserType} {MapMonsterId} {MapX} {MapY}
+        this.lastSkillRequest = s;
+        n.send("u_s "+s.CastId+" 3 "+m.id+" "+n.GameData.Character.Pos.x+" "+n.GameData.Character.Pos.y);
     }
     
     public void onReceive(String packet)
@@ -29,12 +29,39 @@ public class BattlePacketHandler extends Handler{
             if(pac.getInt(2)==n.GameData.Character.id)
             {
                //TODO Parse cooldowns etc..
+               n.GameData.Character.Skills.get(); //get by VNUM
             }
                 break;
+
             
             case "su": //Somebody attacked, get info from that
             //su 1 {hitRequest.Session.Character.CharacterId} 3 {MapMonsterId} {hitRequest.Skill.SkillVNum} {hitRequest.Skill.Cooldown} {hitRequest.Skill.AttackAnimation} {hitRequest.SkillEffect} {hitRequest.Session.Character.PositionX} {hitRequest.Session.Character.PositionY} {(IsAlive ? 1 : 0)} {(int)((float)CurrentHp / (float)Monster.MaxHP * 100)} {damage} {hitmode} {hitRequest.Skill.SkillType - 1}
-             
+                    if(pac.splited[1]=="3") //Monster attacking someone
+                    {
+                  	  if(pac.getInt(4)!=GameData.Character.id){}//If it isnt me who cares?
+                  	  MobAttackedMe(pac.getInt(2));
+                  	  GameData.Character.Hp -= Integer.parseInt(pac.splited[13]);  
+                  	  send("ncif 3 "+pac.getInt(2));
+                    }
+                    else if(pac.splited[1]=="1") //Someone attacking something
+                    {
+                  	  //su 1 {hitRequest.Session.Character.CharacterId} 3 {MapMonsterId} {hitRequest.Skill.SkillVNum} {hitRequest.Skill.Cooldown} {hitRequest.SkillCombo.Animation} {hitRequest.SkillCombo.Effect} {hitRequest.Session.Character.PositionX} {hitRequest.Session.Character.PositionY} {(IsAlive ? 1 : 0)} {(int)((float)CurrentHp / (float)Monster.MaxHP * 100)} {damage} {hitmode} {hitRequest.Skill.SkillType - 1}
+                      //MapMobInstance m = n.GameData.get(pac.getInt(4));
+                      //TODO dodìlat!!!
+                      if(pac.getInt(11)==0) //Monster died
+                      {
+                    	  GameData.Mobs.remove(pac.getInt(4));
+                    	  if(pac.getInt(2)==GameData.Character.id)// I killed it
+                    	  {
+                    		  if(target.id == pac.getInt(4))
+                    		  {
+                                  //if i killed target, null it
+                    			  target = null;
+                    		  }
+                    	  }
+                      }
+                    	  
+                    }
                 break;
                 
               
