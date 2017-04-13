6b492a339211307cb1adeedd537e5d197c8eef7f
lika85456 <lika85456@post.cz>
2017-04-13 18:36:35 +0200

    update
    update

--- nostale2/src/nostale/LogType.java
+++
@@ -1,5 +0,0 @@
-package nostale;
-
-public enum LogType {
-Receive,Send
-}
--- nostale2/src/nostale/login/CServer.java
+++
@@ -1,7 +0,0 @@
-package nostale.login;
-
-//Server ENUM list
-public enum CServer
-{
-    DE, EN, FR, IT, PL, ES, CZ, RU, TR
-}
